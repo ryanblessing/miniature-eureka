31ace9e4a659fd0fca7508e48d3d62b68efff07a
Ryan Blessing <ryanblessing20@yahoo.com>
2021-06-28 08:30:03 -0500

    add heroku for assignment

---
+++ server.js
@@ -0,0 +1,23 @@
+//fs module provides utillities for working w/ file and directory paths
+const fs =  require('fs');
+
+//npm package module
+const express = require('express');
+
+//express function to the app so we can chain on methods to the express server
+const app = express();
+
+
+//get routes to potentially move to a different folder
+app.get('./Develop/db/db.json', (req, res) => {
+    res.json(db.json);
+});
+
+
+
+
+
+// gives us a web page to listen for
+app.listen(3002, () => {
+    console.log('This is actually working?')
+})
