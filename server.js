0edf83bdebc268160beae64c6cafe887d55b0e4d
Ryan Blessing <ryanblessing20@yahoo.com>
2021-06-29 12:09:40 -0500

    middleware

--- server.js
+++ server.js
@@ -1,22 +1,28 @@
 //fs module provides utilities for working w/ file and directory paths
 const fs =  require('fs');
 const path = require('path');
+//npm package module
+const express = require('express');
+
+
+//assigned to chain methods together
+const app = express();
+//express function to the app so we can chain on methods to the express server
+const PORT = process.env.PORT || 3000;
+
+
 //added for routes to api and html
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
-//npm package module
-const express = require('express');
-//express function to the app so we can chain on methods to the express server
-const PORT = process.env.PORT || 3000;
-//assigned to chain methods together
-const app = express();
 
-
+//express.static is our middleware
+app.use(express.static('public'));
+app.use(express.json());
 
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
-//middleware
+
 
 
 // gives us a web page to listen for
